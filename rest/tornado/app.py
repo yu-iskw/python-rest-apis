90229fe0181fb1839b93a2bd2c7568dbf26503a7
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2018-11-27 15:33:17 -0800

    Integrate IrisModel with tornado REST API

--- rest/tornado/app.py
+++ rest/tornado/app.py
@@ -2,25 +2,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import os
+
+import status
+import numpy as np
+
 import tornado.ioloop
 from tornado import web
 from tornado.options import define, options
+
+from rest.model.iris import IrisModel
+from rest.utils import get_project_dir
+
+# Load model
+model_path = os.path.join(get_project_dir(), 'model', 'iris.joblib')
+model = IrisModel(model_path=model_path)
 
 
 class IrisPredictHandler(web.RequestHandler):
     # SUPPORTED_METHODS = ("POST")
 
     def post(self):
-        response = {
-            'message': "hello"
-        }
-        self.set_status(200)
-        self.write(response)
+        response = {}
+        try:
+            sepal_length = self.get_argument('sepal_length')
+            sepal_width = self.get_argument('sepal_width')
+            petal_length = self.get_argument('petal_length')
+            petal_width = self.get_argument('petal_width')
+
+            X = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+            result = model.predict(X)
+            response = result
+            self.set_status(status.HTTP_200_OK)
+            self.write(response)
+        except Exception as e:
+            response = {"message": e}
+            self.set_status(status.HTTP_400_BAD_REQUEST)
+            self.write(response)
 
 
 def make_app(host='localhost'):
     return tornado.web.Application([
-        (r"/", IrisPredictHandler),
+        (r"/v1/predict", IrisPredictHandler),
     ], default_host=host)
 
 
--- tests/tornado/test_app.py
+++ tests/tornado/test_app.py
@@ -2,23 +2,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import json
+import status
+
+from tornado.testing import AsyncHTTPTestCase
+from rest.tornado import app
+
 # compatible importing urllib for python2 and 3.
 from future.standard_library import install_aliases
 install_aliases()
 from urllib.parse import urlencode  # noqa: E402
 
-from tornado.testing import AsyncHTTPTestCase  # noqa: E402
-from rest.tornado import app  # noqa: E402
 
-
-class TestMainHandler(AsyncHTTPTestCase):
+class TestIrisPredictorHandler(AsyncHTTPTestCase):
 
     def get_app(self):
         return app.make_app()
 
     def test_homepage(self):
-        data = {}
+        data = {
+            'sepal_length': 5.1,
+            'sepal_width': 3.3,
+            'petal_length': 1.7,
+            'petal_width': 0.5,
+        }
         data = urlencode(data)
-        response = self.fetch('/', method='POST', body=data)
-        self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, b'{"message": "hello"}')
+        response = self.fetch('/v1/predict', method='POST', body=data)
+        response_json = json.loads(response.body)
+        assert response.code == status.HTTP_200_OK
+        assert response_json['prediction'] == 0
+        assert response_json['inference_time'] is not None
