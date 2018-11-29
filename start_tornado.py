10e3e179c55e700043d55fb638e3196da5b75a44
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2018-11-28 23:12:58 -0800

    Support multiple CPUs

--- start_tornado.py
+++ start_tornado.py
@@ -1,4 +1,4 @@
-from tornado import ioloop
+from tornado import ioloop, httpserver
 from tornado.options import define, options
 
 from rest.tornado_app.app import make_app
@@ -7,8 +7,18 @@ from rest.tornado_app.app import make_app
 if __name__ == "__main__":
     define('host', default='localhost', help='host')
     define('port', default=8080, help='port to listen on')
-    define('restart_interval', default=2, help='interval seconds to restart server.')
+    define('num_processes', default=0, 'number of processes')
 
-    application = make_app(host=options.host)
-    server = application.listen(options.port)
+    # Single CPU
+    # application = make_app(host=options.host)
+    # server = application.listen(options.port)
+    # ioloop.IOLoop.current().start()
+
+    # Multiple CPUs
+    # SEE: https://www.tornadoweb.org/en/stable/guide/running.html#processes-and-ports
+    app = make_app(host=options.host)
+    server = httpserver.HTTPServer(app)
+    server.bind(options.port)
+    # if num_proccesses is 0, forks one process per cpu
+    server.start(options.num_processes)
     ioloop.IOLoop.current().start()
